5633b17111d4e88de59c10527e352904500658b3
25030024-beep <25030024@itesa.edu.mx>
2026-09-08 16:30:54 -0600

    Version_02:Recursividades

---
+++ Recursividad.Logica/Recursividades.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recursividad.Logica
+{
+    public class Recursividades
+    {
+
+            public int CalcularFactorial(int num)
+            {
+                //Caso base condición de salida
+
+                if (num == 1)
+                {
+                    return 1;
+                }
+
+                //Caso general
+                return num = num * CalcularFactorial(num - 1);
+
+
+
+            }
+
+            //public int SumarNumeros(int num)
+            //{
+            //    //Caso Base o salida
+
+
+            //    //Caso general
+            //}
+
+
+        
+    }
+}
---
+++ Recursividad.UI.Consola/Program.cs
@@ -0,0 +1,9 @@
+﻿using Recursividad.Logica;
+
+Console.WriteLine("Ejemplos de recursividad");
+Console.WriteLine("\n\nFactorial");
+
+Recursividades oRecursividades = new Recursividades();
+
+int resultadoFactorial = oRecursividades.CalcularFactorial(5);
+Console.WriteLine("El resultado de 5! es: " + resultadoFactorial);
